c33b90eb275b26ee769494629da3c0a03a0892ed
flostevebron <eravillesteve@gmail.com>
2020-01-22 23:11:47 +0100

    Change source domain

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,106 @@ import React from 'react';
 
 import Layout from '../components/Layout/Layout';
 import Seo from '../components/Seo/Seo';
+import GridContainer from '../components/Grid/GridContainer';
+import GridItem from '../components/Grid/GridItem';
+import classNames from 'classnames';
+import Parallax from '../components/Parallax/Parallax';
+import { GatsbyLink } from 'gatsby-theme-material-ui';
+import Button from '../components/CustomButtons/Button';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import { graphql, useStaticQuery } from 'gatsby';
 
-const NotFoundPage = () => (
-    <Layout>
-        <Seo title="404: Not found" />
-        <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Layout>
-);
+const NotFoundPage = () => {
+    const data = useStaticQuery(graphql`
+        query NonTrouve {
+            wpgraphql {
+                pageBy(pageId: 312) {
+                    banniere {
+                        titreDeLaPage
+                        imageDeBanniere {
+                            sourceUrl
+                            imageFile {
+                                childImageSharp {
+                                    fluid(maxWidth: 1600, maxHeight: 1200) {
+                                        ...GatsbyImageSharpFluid_withWebp
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    contenu_page {
+                        titreDuParagraphe
+                        contenuDuParagraphe
+                    }
+                }
+            }
+        }
+    `);
+
+    return (
+        <Layout>
+            <Seo title="404: Page non trouvée" />
+            <Parallax
+                image={
+                    data.wpgraphql.pageBy.banniere.imageDeBanniere.imageFile
+                        .childImageSharp.fluid.src
+                }
+                className="parallax"
+                filter="dark"
+                small
+            >
+                <div className={classes.container}>
+                    <GridContainer justify="center">
+                        <GridItem
+                            md={8}
+                            sm={8}
+                            className={classNames(
+                                classes.mlAuto,
+                                classes.mrAuto,
+                                classes.textCenter
+                            )}
+                        >
+                            <h1 className={classes.title}>
+                                {data.wpgraphql.pageBy.banniere.titreDeLaPage}
+                            </h1>
+                        </GridItem>
+                    </GridContainer>
+                </div>
+            </Parallax>
+            <div className={classNames(classes.main, classes.mainRaised)}>
+                <div className={classes.container}>
+                    <GridContainer className="page-gsi succes">
+                        <GridItem
+                            xs={12}
+                            className={classes.mlAuto + ' ' + classes.mrAuto}
+                        >
+                            <h2>
+                                {
+                                    data.wpgraphql.pageBy.contenu_page
+                                        .titreDuParagraphe
+                                }
+                            </h2>
+                            <div
+                                dangerouslySetInnerHTML={{
+                                    __html:
+                                        data.wpgraphql.pageBy.contenu_page
+                                            .contenuDuParagraphe,
+                                }}
+                            />
+                            <GatsbyLink to="/" className="retour-succes">
+                                <Button
+                                    color="danger"
+                                    startIcon={<ArrowBackIcon />}
+                                >
+                                    Retour Accueil
+                                </Button>
+                            </GatsbyLink>
+                        </GridItem>
+                    </GridContainer>
+                </div>
+            </div>
+        </Layout>
+    );
+};
 
 export default NotFoundPage;
